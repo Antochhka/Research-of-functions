6cc4c8bc98b0c4318264be029ddc491c81d7951f
Antochhka <antonio-shpakov1@yandex.ru>
2024-09-24 20:51:11 +0300

    adding the is_digit function

--- src/parser.h
+++ src/parser.h
@@ -2,11 +2,21 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <cctype>
 #include <iostream>
+#include <string>
 #include <vector>
+
+#define BUFFER 100
+#define NUM 0
+#define NONE 0
+#define X 1
 
 using namespace std;
 
 void parser();
+void print_arr(vector<vector<double>> &output_arr, unsigned int &i_out_arr);
+unsigned int is_digit(unsigned int &i, string &input, double &sum, vector<vector<double>> &output_arr, unsigned int &i_out_arr);
+
 
 #endif  // PARSER_H
